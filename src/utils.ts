d6f2f30e5a0bf21db93cc768dfbd9f69fd2c8d5b
shivghai <8965168+shivghai@users.noreply.github.com>
2024-07-22 11:51:32 -0400

    Update README, add validation for API key being present

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,9 @@ export const getAuthHeader = (): { Authorization: string } => {
   const ultralightApiKey = process.env.UL_API_KEY
     ? process.env.UL_API_KEY
     : core.getInput('ultralight-api-key')
+  if (!ultralightApiKey) {
+    throw new Error('Ultralight API key is required')
+  }
   return {
     Authorization: `Bearer ${ultralightApiKey}`
   }
